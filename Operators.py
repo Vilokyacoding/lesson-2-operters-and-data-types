606f18a6291299435220d23c226ca828a1bea1bf
Vilokyacoding <vilokya2025@gmail.com>
2025-05-12 21:52:43 -0400

    operators

---
+++ Operators.py
@@ -0,0 +1,19 @@
+num1=2
+num2=4
+print(num1)
+print(num2)
+print("addition", num1+num2)
+print("subtraction", num1-num2)
+print("multiplication", num1*num2)
+print("division", num1/num2)
+print("floor division", num1//num2)
+print("modulus", num1%num2)
+print("exponent", num1**num2)
+# Comparison Operators
+print("Comparison Operators")
+print(num1==num2)
+print(num1!=num2)
+print(num1>num2)
+print(num1<num2)
+print(num1>=num2)
+print(num1<=num2)
