2c217502fde2443ff5c0a7c371ecc4d0b95e9286
Vilokyacoding <vilokya2025@gmail.com>
2025-05-12 22:03:34 -0400

    Play withstrings

---
+++ Play With Strings.py
@@ -0,0 +1,10 @@
+firstname="VilokyaChandra"
+lastname="Varnavie"
+print(firstname)
+print(lastname)
+print("addition", firstname+lastname)
+print(firstname*5)
+print(firstname[0])
+print(len(firstname))
+print(firstname[0:7])
+print(firstname[-1])
